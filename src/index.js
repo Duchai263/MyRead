eca90fce110b63b6f824d83b1ca4103ee9c4e9ef
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2023-11-06 14:45:45 +0700

    Restructure everything

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import App from "./js/App";
+// import Test from "./App_Test";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let shelves = [{name:"Currently Reading", key:"currentlyReading"} , {name: "Want to Read", key: "wantToRead"},{name: "Read", key:"read"}]
+                
+root.render(<App shelves={shelves}/>)
